a38b1d764826d88dd03e4d97b6738f88a570f398
Aurezz <31102170+Aurezz@users.noreply.github.com>
2018-01-01 19:45:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
+  client.user.setPresence({game: {name: "Under Development!", type: 0}});
   console.log('I am ready!');
 });
 
@@ -13,41 +14,6 @@ client.on('message', (message, roles) => {
   if (message.content === prefix + 'ping') {
     message.channel.send('Pong!')
 }
-  if(message.content === prefix + 'eval') {
-    if(message.author.id !== "133867153890869248") return;
-    const args3 = message.content.split(' ').slice(1);
-    function clean(text) {
-        if (typeof (text) === 'string')
-            return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
-        else
-            return text;
 
-    }
-    const code = args.join(' ');
-    try {
-        let evaled = eval(code);
-        if (typeof evaled !== 'string')
-            evaled = require('util').inspect(evaled);
-        if (evaled.includes(client.token)) evaled = evaled.replace(client.token, `NANI!`);
-        if (code.startsWith("//")) evaled = evaled.replace("undefined", "Umm...is that just supposed to be 2 backslashes?!!")
-        if (code.includes("rm")) return;
-        const embed = new Discord.RichEmbed()
-            .setTitle(`\`Evaled Code\``)
-            .setAuthor('Worked!')
-            .setColor(0x00ff00)
-            .addField(':inbox_tray: INPUT', "**```js\n" + code + "```**")
-            .addField(':outbox_tray: OUTPUT', "**```js\n" + clean(evaled) + "```**")
-        message.channel.send({ embed });
-    } catch (err) {
-        const embed = new Discord.RichEmbed()
-            .setTitle('`Evaled Code`')
-            .setColor(0xff0000)
-            .setAuthor("Oops! Looks like there's an error!")
-            .addField(':x:', "**```js\n" + code + "```**")
-            .addField(':x:', "**```js\n" + clean(err) + "```**")
-        message.channel.send({ embed });
- }
-}
-});
     
 client.login(process.env.BOT_TOKEN);
