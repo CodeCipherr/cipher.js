6bf3c5f42941a4cbf74da633e55ef184f141dfd3
Aurezz <31102170+Aurezz@users.noreply.github.com>
2018-01-01 18:56:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
-  }
-});
+const prefix = '>'
+client.on('message', (message, roles) => {
+  if (message.author.bot) return;
+  if (!command.startsWith(prefix)) return;
 
+  if (command === prefix + 'ping') {
+    message.channel.send('Pong!')
+}
+});
+    
 client.login(process.env.BOT_TOKEN);
