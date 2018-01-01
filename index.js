6359ef0d09a4d59a4bd52ced684155b72fb85ce5
Aurezz <31102170+Aurezz@users.noreply.github.com>
2018-01-01 19:50:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setPresence({game: {name: "Under Development!", type: 0}});
+  client.user.setPresence({game: {name: ("Under Development!"), type: 0}});
   console.log('I am ready!');
 });
 
