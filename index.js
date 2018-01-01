ecc892cf4cdad168a26d36e00907f4bfee2a3db5
Aurezz <31102170+Aurezz@users.noreply.github.com>
2018-01-01 19:52:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 });
 
 const prefix = '>'
-client.on('message', (message, roles) => {
+client.on('message', message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
 
