fef5a23b238786e1c0f1f09aa6c99a9782c45655
Aurezz <31102170+Aurezz@users.noreply.github.com>
2018-01-01 18:02:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log('Bot ready to work!');
+});
+
+const prefix = '>'
+client.on('message', (message, roles) => {
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+  if (message.content.startsWith === prefix + 'ping') {
+  message.channel.send('Pong!');
+  }};
+          
+  client.login(process.env.BOT_TOKEN);
